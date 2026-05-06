cc5b71519dc0a10619a6ba0da570111004e6e94f
Do Tran Thai An <justapie06@gmail.com>
2026-05-06 14:03:30 +0700

    feat: add ticket_service to app_state

--- src/app_state.rs
+++ src/app_state.rs
@@ -2,7 +2,10 @@ use std::sync::{Arc, Mutex, atomic::AtomicBool};
 
 use rusqlite::Connection;
 
+use crate::ticket::application::TicketService;
+
 pub struct AppState {
     pub conn: Arc<Mutex<Connection>>,
     pub running: AtomicBool,
+    pub ticket_service: Arc<TicketService>,
 }
--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,18 @@ use std::io::Result;
 use std::sync::atomic::AtomicBool;
 use std::sync::{Arc, Mutex};
 
+use crate::ticket::application::TicketService;
 use crate::{app::App, app_state::AppState};
 
 fn main() -> Result<()> {
-    let conn: Connection = Connection::open("data.db").expect("data.db must be created");
+    let conn: Arc<Mutex<Connection>> = Arc::new(Mutex::new(
+        Connection::open("data.db").expect("data.db must be created"),
+    ));
 
     let state: Arc<AppState> = Arc::new(AppState {
-        conn: Arc::new(Mutex::new(conn)),
+        conn: conn.clone(),
         running: AtomicBool::from(true),
+        ticket_service: Arc::new(TicketService::new(conn.clone())),
     });
 
     ratatui::run(|term| App::new(state).run(term))
